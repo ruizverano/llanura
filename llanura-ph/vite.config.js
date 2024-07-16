a657618f06aecb6d9e8b4239e98cf36c4f8c26cc
ruizverano <cristianmr30@hotmail.com>
2024-07-15 20:22:50 -0500

    cambio de ip para node

--- llanura-ph/vite.config.js
+++ llanura-ph/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     server: {
-        host: '0.0.0.0', // Escucha en todas las interfaces
+        //host: '0.0.0.0', // Escucha en todas las interfaces
+        host: '4.228.227.54', // Escucha en todas las interfaces
         port: 5173,      // Puerto de Vite
     },
     plugins: [
