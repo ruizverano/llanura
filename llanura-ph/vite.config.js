c4bd045707dddf060fd0acfabe892179eb0c0b69
ruizverano <cristianmr30@hotmail.com>
2024-07-15 20:28:07 -0500

    volviendo a vite con servidor

--- llanura-ph/vite.config.js
+++ llanura-ph/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     server: {
-        //host: '0.0.0.0', // Escucha en todas las interfaces
-        host: '127.0.0.1',
+        host: '0.0.0.0', // Escucha en todas las interfaces
+        //host: '127.0.0.1',
         port: 5173,      // Puerto de Vite
     },
     plugins: [
