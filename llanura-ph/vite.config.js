216b8c14f189b23e941ddd5927c729f29cbb045b
Ubuntu <ruiz@alvarado.zwl3f2g2qbje3grv0nhm1hde2a.nx.internal.cloudapp.net>
2024-07-16 14:49:52 +0000

    experimento con middleware para cors

--- llanura-ph/vite.config.js
+++ llanura-ph/vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+	 server: {
+	    host: '0.0.0.0',
+	    port: 5173
+	  },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
