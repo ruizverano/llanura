fda75725e36ae30110e7cb7078c58d24bb3c6e54
ruizverano <cristianmr30@hotmail.com>
2024-07-16 08:57:33 -0500

    vite con proxy

--- llanura-ph/vite.config.js
+++ llanura-ph/vite.config.js
@@ -3,6 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        proxy: {
+            '/': {
+                target: 'http://4.228.227.54:8000', // Ajusta según tu configuración de APP_URL en Laravel
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
