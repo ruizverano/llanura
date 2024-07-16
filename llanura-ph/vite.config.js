d9327e103a171bbe3efd08ef519ab22017f374dd
ruizverano <cristianmr30@hotmail.com>
2024-07-16 13:56:52 -0500

    puerto 80 para vite

--- llanura-ph/vite.config.js
+++ llanura-ph/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     server: {
 	    host: '0.0.0.0',
-	    port: 5173
+	    port: 80
 	  },
     plugins: [
         laravel({
