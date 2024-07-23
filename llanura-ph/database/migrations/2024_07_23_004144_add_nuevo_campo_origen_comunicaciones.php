1940705e4761d81e7a033c0db118ce2a7b1abb3f
ruizverano <cristianmr30@hotmail.com>
2024-07-22 19:51:25 -0500

    nuevo campo origen en tabla comunicaciones

--- llanura-ph/database/migrations/2024_07_23_004144_add_nuevo_campo_origen_comunicaciones.php
+++ llanura-ph/database/migrations/2024_07_23_004144_add_nuevo_campo_origen_comunicaciones.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('comunicaciones', function (Blueprint $table) {
-            //
+            $table->string('origen')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('comunicaciones', function (Blueprint $table) {
-            //
+            $table->dropColumn('origen');
         });
     }
 };
