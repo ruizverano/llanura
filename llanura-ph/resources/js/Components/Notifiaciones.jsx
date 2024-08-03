7e7c5a4884bc1a3388739e08d751a408ac712eb8
ruizverano <cristianmr30@hotmail.com>
2024-08-03 17:36:56 -0500

    cambios notificacion vbox

--- llanura-ph/resources/js/Components/Notifiaciones.jsx
+++ llanura-ph/resources/js/Components/Notifiaciones.jsx
@@ -1,57 +1,63 @@
 import { Button } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 
-const otorgarPermisos = () => {
-    if ('Notification' in window) {
-        // Solicitar permiso para notificaciones
-        Notification.requestPermission().then(permission => {
-            if (permission === 'granted') {
-                console.log('Notificaciones permitidas');
-            } else {
-                console.log('Notificaciones denegadas');
-            }
-        });
-    } else {
-        console.log('Notificaciones no soportadas en este navegador');
-    }
-}
-
-const sendNotification = () => {
-    if (Notification.permission === 'granted') {
-        new Notification('¡Hola!', {
-            body: 'Esto es una notificación de prueba.',
-            icon: 'https://example.com/icon.png'
-        });
-    } else {
-        console.log('Permiso para notificaciones no concedido');
-    }
-};
-
 export default function Notificaciones() {
 
-
+    const [permission, setPermission] = useState(Notification.permission);
+    
+    const otorgarPermisos = () => {        
+    
+        if ('Notification' in window) {
+            // Solicitar permiso para notificaciones si no ha sido otorgado o denegado
+            if (Notification.permission === 'default') {
+                Notification.requestPermission().then(permission => {
+                    setPermission(permission);
+                    if (permission === 'granted') {
+                        alert('Notificaciones permitidas');
+                    } else {
+                        alert('Notificaciones denegadas');
+                    }
+                });
+            } else {
+                setPermission(Notification.permission);
+            }
+        } else {
+            alert('Notificaciones no soportadas en este navegador');
+        }
+    }
+    
+    const sendNotification = () => {
+        if (permission === 'granted') {
+            new Notification('¡Hola!', {
+                body: 'Esto es una notificación de prueba.',
+                icon: 'https://example.com/icon.png'
+            });
+        } else {
+            console.log('Permiso para notificaciones no concedido');
+        }
+    };
+    
     useEffect(() => {
-        otorgarPermisos();        
-        alert('entra');
+        otorgarPermisos();
     }, [])
 
     return (
         <Button
-        variant='contained'
-        onClick={sendNotification}
-        sx={{
-            backgroundColor: '#000000',
-            color: '#FFFFFF',
-            '&:hover': {
-                backgroundColor: '#333333',
-            },
-            padding: '10px 20px',
-            borderRadius: '5px',
-            fontSize: '16px',
-            textTransform: 'none',  // Para que el texto no esté en mayúsculas
-        }}
-    >
-        Notificacion
-    </Button>
+            variant='contained'
+            onClick={sendNotification}
+            sx={{
+                backgroundColor: '#000000',
+                color: '#FFFFFF',
+                '&:hover': {
+                    backgroundColor: '#333333',
+                },
+                padding: '10px 20px',
+                borderRadius: '5px',
+                fontSize: '16px',
+                textTransform: 'none',  // Para que el texto no esté en mayúsculas
+            }}
+        >
+            Notificacion
+        </Button>
     );
 }
